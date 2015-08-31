51925c6f90969ffcdf6733b03b4e243ce5478402
cat8dog <catherine.reyto@gmail.com>
2015-08-31 16:24:33 -0400

    Stage 2 completed

--- Kindling/Kindling/RootPresentationViewController.swift
+++ Kindling/Kindling/RootPresentationViewController.swift
@@ -2,6 +2,10 @@ import UIKit
 
 class RootPresentationViewController: UIViewController {
 
+    @IBOutlet weak var cardContainerView: UIView!
+    
+    
+    @IBOutlet weak var horizontalCenterConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +20,34 @@ class RootPresentationViewController: UIViewController {
       //  var gesture = UIPanGestureRecognizer(target: self, action: Selector ("wasSwipped:"))
     }
     
+    @IBAction func didPan(sender: UIPanGestureRecognizer) {
+        
+        switch sender.state {
+            
+        case .Began :
+            
+            println ("pan did begin")
+            
+        case .Changed :
+            var panVector = sender.translationInView(view)
+            if abs(panVector.x) < self.view.bounds.width / 2 {
+
+            horizontalCenterConstraint.constant = -panVector.x
+            
+            println ("pan changed")
+            }
+            
+        case .Ended :
+            
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                self.horizontalCenterConstraint.constant = 0
+                self.view.layoutIfNeeded()
+            })
+            
+        default:
+            println("default selection")
+        }
+    }
 
     /*
     // MARK: - Navigation
